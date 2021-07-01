89d97f23c0a53d1f20891f0001e7a0aff5fb75f9
ramprits <palak.ghai@torinit.ca>
2021-07-01 19:09:43 +0530

    Adding env

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   mongoose.connect(
-    process.env.MONGO_URI || "mongodb://localhost:27017/jwtAppDB",
+    process.env.MONGO_URI,
     {
       useNewUrlParser: true,
       useCreateIndex: true,
